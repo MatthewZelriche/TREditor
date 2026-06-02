be0d1e23a6c56473f459c7c30bf9a1e0caf8c21f
MatthewZelriche <67518084+MatthewZelriche@users.noreply.github.com>
2026-06-01 18:33:10 -0600

    Matcap Shader

--- scripts/rendering/MeshRenderable.cs
+++ scripts/rendering/MeshRenderable.cs
@@ -7,6 +7,9 @@ using NumericsVector3 = System.Numerics.Vector3;
 
 public partial class MeshRenderable : MeshInstance3D
 {
+    private const string DefaultMaterialPath = "res://resource/matcap_material.tres";
+    private static Material _defaultMaterial;
+
     public SpatialMesh SourceMesh { get; private set; } = new();
 
     // Can't have parameter constructors for Godot, so this is like a re-usable constructor.
@@ -26,9 +29,10 @@ public partial class MeshRenderable : MeshInstance3D
 
     public void RebuildRenderableMesh()
     {
+        var vertices = new List<Vector3>();
+        var normals = new List<Vector3>();
         var indices = new List<int>();
         var faceIndices = new List<int>();
-        int highestDenseIndex = -1;
 
         foreach (var face in SourceMesh.EnumerateLiveFaces())
         {
@@ -47,10 +51,7 @@ public partial class MeshRenderable : MeshInstance3D
                 int c = faceIndices[i + 2];
 
                 // TRMesh stores outward faces CCW; Godot expects the opposite winding here.
-                indices.Add(a);
-                indices.Add(c);
-                indices.Add(b);
-                highestDenseIndex = Math.Max(highestDenseIndex, Math.Max(a, Math.Max(b, c)));
+                AddRenderTriangle(vertices, normals, indices, a, c, b);
             }
         }
 
@@ -60,29 +61,16 @@ public partial class MeshRenderable : MeshInstance3D
             return;
         }
 
-        // TriangulateFace returns dense vertex indices, so build the Godot arrays in dense order.
-        // TODO: Consider copying over the position data directly to avoid intermediate allocations.
-        // TODO: Consider hoisting normal computation into TRMesh library.
-        int vertexCount = highestDenseIndex + 1;
-        var vertices = new Vector3[vertexCount];
-        for (int i = 0; i < vertexCount; i++)
-        {
-            vertices[i] = ToGodotVector3(SourceMesh.GetVertexPositionByDenseIndex(i));
-        }
-
-        // TODO: This ToArray is costly.
-        int[] indexArray = [.. indices];
-        Vector3[] normals = CalculateVertexNormals(vertices, indexArray);
-
         var meshArrays = new GodotArray();
         meshArrays.Resize((int)Godot.Mesh.ArrayType.Max);
-        meshArrays[(int)Godot.Mesh.ArrayType.Vertex] = vertices;
-        meshArrays[(int)Godot.Mesh.ArrayType.Normal] = normals;
-        meshArrays[(int)Godot.Mesh.ArrayType.Index] = indexArray;
+        meshArrays[(int)Godot.Mesh.ArrayType.Vertex] = vertices.ToArray();
+        meshArrays[(int)Godot.Mesh.ArrayType.Normal] = normals.ToArray();
+        meshArrays[(int)Godot.Mesh.ArrayType.Index] = indices.ToArray();
 
         var renderMesh = new ArrayMesh();
         renderMesh.AddSurfaceFromArrays(Godot.Mesh.PrimitiveType.Triangles, meshArrays);
         Mesh = renderMesh;
+        ApplyDefaultMaterialIfNeeded();
     }
 
     public override void _Notification(int what)
@@ -96,31 +84,55 @@ public partial class MeshRenderable : MeshInstance3D
     private static Vector3 ToGodotVector3(NumericsVector3 vector) =>
         new(vector.X, vector.Y, vector.Z);
 
-    private static Vector3[] CalculateVertexNormals(Vector3[] vertices, int[] indices)
+    private void AddRenderTriangle(
+        List<Vector3> vertices,
+        List<Vector3> normals,
+        List<int> indices,
+        int aIndex,
+        int bIndex,
+        int cIndex
+    )
     {
-        var normals = new Vector3[vertices.Length];
+        Vector3 a = ToGodotVector3(SourceMesh.GetVertexPositionByDenseIndex(aIndex));
+        Vector3 b = ToGodotVector3(SourceMesh.GetVertexPositionByDenseIndex(bIndex));
+        Vector3 c = ToGodotVector3(SourceMesh.GetVertexPositionByDenseIndex(cIndex));
+        Vector3 normal = CalculateTriangleNormal(a, b, c);
+        int firstRenderIndex = vertices.Count;
 
-        for (int i = 0; i < indices.Length; i += 3)
-        {
-            int aIndex = indices[i];
-            int bIndex = indices[i + 1];
-            int cIndex = indices[i + 2];
-
-            Vector3 a = vertices[aIndex];
-            Vector3 b = vertices[bIndex];
-            Vector3 c = vertices[cIndex];
-            Vector3 triangleNormal = (b - a).Cross(c - a);
-
-            normals[aIndex] += triangleNormal;
-            normals[bIndex] += triangleNormal;
-            normals[cIndex] += triangleNormal;
-        }
-
-        for (int i = 0; i < normals.Length; i++)
-        {
-            normals[i] = normals[i].LengthSquared() > 0.0f ? normals[i].Normalized() : Vector3.Up;
-        }
-
-        return normals;
+        vertices.Add(a);
+        vertices.Add(b);
+        vertices.Add(c);
+        normals.Add(normal);
+        normals.Add(normal);
+        normals.Add(normal);
+        indices.Add(firstRenderIndex);
+        indices.Add(firstRenderIndex + 1);
+        indices.Add(firstRenderIndex + 2);
     }
+
+    private static Vector3 CalculateTriangleNormal(Vector3 a, Vector3 b, Vector3 c)
+    {
+        Vector3 normal = (b - a).Cross(c - a);
+        return normal.LengthSquared() > 0.0f ? normal.Normalized() : Vector3.Up;
+    }
+
+    private void ApplyDefaultMaterialIfNeeded()
+    {
+        if (MaterialOverride != null)
+        {
+            return;
+        }
+
+        _defaultMaterial ??= ResourceLoader.Load<Material>(DefaultMaterialPath);
+        if (_defaultMaterial == null)
+        {
+            GD.PushWarning(
+                $"MeshRenderable could not load default material: {DefaultMaterialPath}"
+            );
+            return;
+        }
+
+        MaterialOverride = _defaultMaterial;
+    }
+
 }
